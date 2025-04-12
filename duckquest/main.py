7eb6f79492a7b03d3436e722811b4a989ee9d845
Léo Leman <leo.leman555@gmail.com>
2025-04-12 10:36:27 +0200

    chore(build): add initial packaging configuration for PyPI distribution

--- duckquest/main.py
+++ duckquest/main.py
@@ -1,16 +1,22 @@
 import platform
 from duckquest.utils.helpers import is_raspberry_pi
 
-OS_NAME = platform.system()
-ON_RASPBERRY_PI = is_raspberry_pi()
 
-print(f"Starting game on {OS_NAME}")
-if ON_RASPBERRY_PI:
-    print("Running on Raspberry Pi hardware.")
-else:
-    print("Running in mock mode (Non-Raspberry Pi system).")
+def main():
+    OS_NAME = platform.system()
+    ON_RASPBERRY_PI = is_raspberry_pi()
 
-from duckquest.game_manager import GameManager
+    print(f"Starting game on {OS_NAME}")
+    if ON_RASPBERRY_PI:
+        print("Running on Raspberry Pi hardware.")
+    else:
+        print("Running in mock mode (Non-Raspberry Pi system).")
 
-game = GameManager(ON_RASPBERRY_PI)
-game.root.mainloop()
+    from duckquest.game_manager import GameManager
+
+    game = GameManager(ON_RASPBERRY_PI)
+    game.root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
